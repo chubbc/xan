182674f70094d67106f8090f7ab7623e07900158
Christopher T. Chubb <cchu4043@gmail.com>
2025-11-18 00:45:12 +1100

    Fixed vite nonsense

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,26 @@
-// vite.config.js
-export default {
-  base: '/xan/',
-}
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import fs from 'fs'
+
+export default defineConfig(() => {
+  // Get all HTML files in project root
+  const htmlFiles = fs.readdirSync(__dirname)
+    .filter(file => file.endsWith('.html'))
+
+  // Turn them into a Rollup input map
+  const input = Object.fromEntries(
+    htmlFiles.map(file => [
+      file.replace('.html', ''), // name without extension
+      resolve(__dirname, file)
+    ])
+  )
+
+  return {
+    base: '/xan/',
+    build: {
+      rollupOptions: {
+        input
+      }
+    }
+  }
+})
